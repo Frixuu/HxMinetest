fa4dcc9f939067ce63ad7e8fe4a70162118069a4
Frixuu <kontakt@lukasz.xyz>
2022-08-02 14:47:39 +0200

    Add a fake Player extern

---
+++ src/minetest/data/PlayerRef.hx
@@ -0,0 +1,6 @@
+package minetest.data;
+
+/**
+	An `ObjectRef` that is known to represent a player.
+**/
+extern abstract class PlayerRef extends ObjectRef {}
---
+++ src/minetest/data/PlayerRefExtensions.hx
@@ -0,0 +1,17 @@
+package minetest.data;
+
+/**
+	These extensions provide idiomatic functions for the player object
+	that should be easier to use than their plain Lua counterparts.
+**/
+class PlayerRefExtensions {
+	/**
+		Sends a chat message to that player.
+	**/
+	static public function sendChatMessage(player:PlayerRef, text:String) {
+		final playerName = player.getPlayerName();
+		if (playerName != "") {
+			Minetest.chatSendPlayer(playerName, text);
+		}
+	}
+}
