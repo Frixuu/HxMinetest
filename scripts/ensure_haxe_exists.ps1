3f165e0ea2f9111ae9885ab5617baa4ae6941a53
Frixuu <kontakt@lukasz.xyz>
2023-04-07 02:47:35 +0200

    Update version checker to match 4.3.0 nightlies

--- scripts/ensure_haxe_exists.ps1
+++ scripts/ensure_haxe_exists.ps1
@@ -4,10 +4,9 @@ using namespace System.Management.Automation
 
 [ApplicationInfo]$haxe = Get-Command "haxe" -CommandType Application -ErrorAction SilentlyContinue
 if ($haxe) {
-    $minVersion = [SemanticVersion]::new(4, 3, 0)
     $version = [SemanticVersion]::new(@(haxe --version)[0]);
-    if ($version -lt $minVersion) {
-        throw "Haxe compiler found, but it is too old (have: $version, must be: $minVersion)"
+    if ([SemanticVersion]::new(4, 2, 5) -ge $version) {
+        throw "Haxe compiler found, but it is too old (have: $version, must be: 4.3.0)"
     }
 }
 else {
