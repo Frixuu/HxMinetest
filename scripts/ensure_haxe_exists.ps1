e116418182936dda3ac4f389bbe0e82ab85ff9f1
Frixuu <kontakt@lukasz.xyz>
2023-04-07 02:39:38 +0200

    Allow building with 4.3.0 and up

--- scripts/ensure_haxe_exists.ps1
+++ scripts/ensure_haxe_exists.ps1
@@ -4,7 +4,7 @@ using namespace System.Management.Automation
 
 [ApplicationInfo]$haxe = Get-Command "haxe" -CommandType Application -ErrorAction SilentlyContinue
 if ($haxe) {
-    $minVersion = [SemanticVersion]::new(4, 2, 5)
+    $minVersion = [SemanticVersion]::new(4, 3, 0)
     $version = [SemanticVersion]::new(@(haxe --version)[0]);
     if ($version -lt $minVersion) {
         throw "Haxe compiler found, but it is too old (have: $version, must be: $minVersion)"
