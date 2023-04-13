eea7b3c2dc39bf155cdd9e0358ac81c87962cec3
Frixuu <kontakt@lukasz.xyz>
2023-04-13 14:30:46 +0200

    Move partials library to its own fork

--- src/partials/Partial.hx
+++
@@ -1,7 +0,0 @@
-package partials;
-
-/**
- * Implement this interface to indicate that part of the class is defined somewhere else.
- */
-@:autoBuild(partials.Partials.process())
-interface Partial {}
