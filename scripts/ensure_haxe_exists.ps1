eb0d0df0bd67294adb4a7122009e119be31d07bb
Frixuu <kontakt@lukasz.xyz>
2023-04-13 16:02:29 +0200

    Update CI; require PowerShell 7.3

--- scripts/ensure_haxe_exists.ps1
+++ scripts/ensure_haxe_exists.ps1
@@ -1,12 +1,13 @@
-#Requires -Version 7.0
+#Requires -Version 7.3
 
 using namespace System.Management.Automation
 
 [ApplicationInfo]$haxe = Get-Command "haxe" -CommandType Application -ErrorAction SilentlyContinue
 if ($haxe) {
-    $version = [SemanticVersion]::new(@(haxe --version)[0]);
-    if ([SemanticVersion]::new(4, 2, 5) -ge $version) {
-        throw "Haxe compiler found, but it is too old (have: $version, must be: 4.3.0)"
+    $minVersion = [SemanticVersion]::new(4, 3, 0)
+    $version = [SemanticVersion]::Parse(@(haxe --version)[0]);
+    if ($minVersion -gt $version) {
+        throw "Haxe compiler found, but it is too old (have: $version, must be: $minVersion)"
     }
 }
 else {
