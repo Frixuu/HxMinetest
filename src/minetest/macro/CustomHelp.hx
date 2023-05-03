b05e6549f4596226bcbc0c10512c72c3e2066fb5
Frixuu <kontakt@lukasz.xyz>
2023-05-03 16:26:53 +0200

    Try not to register custom defines on Haxe 5

--- src/minetest/macro/CustomHelp.hx
+++ src/minetest/macro/CustomHelp.hx
@@ -5,12 +5,14 @@ import haxe.macro.Compiler;
 
 final class CustomHelp {
     public static function register() {
+        #if (haxe < version("4.3.999"))
         Compiler.registerCustomDefine({
             define: "csm",
             doc: "If defined, HxMinetest will try its best to only enable client-side externs. " +
             "If omitted, it will be configured for regular, server-side modding.",
             platforms: [Lua],
         }, "hxminetest");
+        #end
     }
 }
 #end
