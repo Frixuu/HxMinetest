0067d7eff86a8a64e5bd5320efced045c1d204b8
Frixuu <kontakt@lukasz.xyz>
2023-06-20 18:22:59 +0200

    Update ExampleHttp.hx

--- examples/http/ExampleHttp.hx
+++ examples/http/ExampleHttp.hx
@@ -3,36 +3,40 @@ import minetest.insecure.http.Request;
 import minetest.Minetest;
 
 typedef Quote = {
-    var anime: String;
-    var character: String;
-    var quote: String;
+    var content: String;
+    var author: String;
 }
 
-class ExampleHttp {
-    private static function main() {
+function main() {
 
-        final httpApi: Null<HttpApi> = Minetest.requestHttpApi();
-        if (httpApi == null) {
-            Minetest.log(Error, "Cannot request HTTP API, this example will not work!");
+    final httpApi: Null<HttpApi> = Minetest.requestHttpApi();
+    if (httpApi == null) {
+        Minetest.log(Error, "Cannot request HTTP API, this example will not work!");
+        return;
+    }
+
+    final request = Request.GET("https://api.quotable.io/random"); // not affiliated
+    request.userAgent = "HxMinetest/Example";
+    request.timeout = 5;
+    httpApi.fetch(request).thenAccept(response -> {
+
+        if (!response.succeeded) {
+            Minetest.log(Error, "HTTP request failed");
             return;
         }
 
-        final request = Request.GET("https://animechan.vercel.app/api/random"); // not affiliated
-        request.userAgent = "HxMinetest/Example";
-        request.timeout = 5;
-        httpApi.fetch(request)
-            .thenAccept(response -> {
-                if (response.succeeded) {
-                    try {
-                        final body: Quote = cast Minetest.parseJson(response.data);
-                        Minetest.log(Action, 'As ${body.character} from ${body.anime} once said:');
-                        Minetest.log(Action, body.quote);
-                    } catch (e:Dynamic) {
-                        Minetest.log(Error, 'Could not parse response body: $e');
-                    }
-                } else {
-                    Minetest.log(Warning, "HTTP request failed");
-                }
-            });
-    }
+        if (response.statusCode != 200) {
+            Minetest.log(Error, 'API responded with code ${response.statusCode}');
+            return;
+        }
+
+        final body: Quote = cast Minetest.parseJson(response.data);
+        if (body == null) {
+            Minetest.log(Error, 'Could not parse response body. See logs for details.');
+            return;
+        }
+
+        Minetest.log('As ${body.author} once said:');
+        Minetest.log("\"" + body.content + "\"");
+    });
 }
