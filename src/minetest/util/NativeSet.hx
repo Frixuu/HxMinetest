00b6b4a033b1fc571ad72bd600b3073be0afe655
Frixuu <kontakt@lukasz.xyz>
2023-07-18 15:15:36 +0200

    Add comments to NativeSet

--- src/minetest/util/NativeSet.hx
+++ src/minetest/util/NativeSet.hx
@@ -4,12 +4,31 @@ package minetest.util;
 import lua.Lua;
 import lua.Table;
 
+/**
+ * A collection that contains no duplicate elements.
+ */
 abstract NativeSet<T>(Table<T, Bool>) to Table<T, Bool> {
+
+    /**
+     * Creates a new `NativeSet`.
+     */
+    @:pure
+    public inline function new() {
+        this = Table.create();
+    }
+
+    /**
+     * Checks if an element already exists in the set.
+     */
     @:pure
     public inline function has(value: T): Bool {
         return this[untyped value] == true;
     }
 
+    /**
+     * Adds `value` to the set, if possible.
+     * @return False, if `value` already exists in the set.
+     */
     public inline function insert(value: T): Bool {
         return if (abstract.has(value)) {
             false;
@@ -19,6 +38,10 @@ abstract NativeSet<T>(Table<T, Bool>) to Table<T, Bool> {
         };
     }
 
+    /**
+     * Removes `value` from the set.
+     * @return False if `value` never existed in the set.
+     */
     public inline function remove(value: T): Bool {
         return if (abstract.has(value)) {
             @:nullSafety(Off) this[untyped value] = null;
