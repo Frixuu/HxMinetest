91103a7905db7275154a36d4508ed61638cff5a2
Frixuu <kontakt@lukasz.xyz>
2024-07-23 15:43:58 +0200

    Migrate Vitepress build script to Deno
    This removes dependency not only on pnpm, but also on Node.

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Zlib
-export * as datetime from "https://deno.land/std@0.197.0/datetime/mod.ts";
-export * as path from "https://deno.land/std@0.197.0/path/mod.ts";
 export * as semver from "https://deno.land/x/semver@v1.4.1/mod.ts";
-export * as xml from "https://deno.land/x/xml@2.1.1/mod.ts"
+export * as xml from "https://deno.land/x/xml@2.1.1/mod.ts";
 export { compress } from "https://deno.land/x/zip@v1.2.5/mod.ts";
+export * as datetime from "jsr:@std/datetime@^0.224.3";
+export * as path from "jsr:@std/path@^1.0.1";
+
